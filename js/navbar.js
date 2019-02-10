b431ee629ba0abeb56c8a8d2fa80dab1660c2a6b
tpr18 <tpr18@ic.ac.uk>
2019-02-10 16:36:47 +0000

    Fixed js colours, and fixed references to deleted Contacts page

--- js/navbar.js
+++ js/navbar.js
@@ -1,52 +1,50 @@
 $(document).scroll(function() {
-  if ($(this).scrollTop() >= $("#contact").offset().top - 120) {
+  // if ($(this).scrollTop() >= $("#contact").offset().top - 120) {
+  //   $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
+  // } else if ($(this).scrollTop() >= $("#links").offset().top - 120) {
+  //   $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
+  if ($(this).scrollTop() >= $("#links").offset().top - 120) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
-  } else if ($(this).scrollTop() >= $("#links").offset().top - 120) {
-    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
   } else if ($(this).scrollTop() >= $("#pseudo").offset().top - 120) {
-    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
   } else if ($(this).scrollTop() >= $("#history").offset().top - 120) {
-    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
-  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
+  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
   } else {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
   }
 });
 
 $(document).scroll(function() {
-  if ($(this).scrollTop() >= $("#contact").offset().top - 120) {
+  if ($(this).scrollTop() >= $("#links").offset().top - 120) {
     $('.navbar-brand').css({"color": "#222"});
-  } else if ($(this).scrollTop() >= $("#links").offset().top - 120) {
-    $('.navbar-brand').css({"color": "#f8f9fa"});
   } else if ($(this).scrollTop() >= $("#pseudo").offset().top - 120) {
-    $('.navbar-brand').css({"color": "#222"});
+    $('.navbar-brand').css({"color": "#f8f9fa"});
   } else if ($(this).scrollTop() >= $("#history").offset().top - 120) {
-    $('.navbar-brand').css({"color": "#f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('.navbar-brand').css({"color": "#222"});
-  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('.navbar-brand').css({"color": "#f8f9fa"});
+  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+    $('.navbar-brand').css({"color": "#222"});
   } else {
     $('.navbar-brand').css({"color": "#222"});
   }
 });
 
 $(document).scroll(function() {
-  if ($(this).scrollTop() >= $("#contact").offset().top - 120) {
+  if ($(this).scrollTop() >= $("#links").offset().top - 120) {
     $('nav').css({"background-color": "#f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#links").offset().top - 120) {
-    $('nav').css({"background-color": "#222"});
   } else if ($(this).scrollTop() >= $("#pseudo").offset().top - 120) {
-    $('nav').css({"background-color": "#f8f9fa"});
+    $('nav').css({"background-color": "#222"});
   } else if ($(this).scrollTop() >= $("#history").offset().top - 120) {
-    $('nav').css({"background-color": "#222"});
-  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('nav').css({"background-color": "#f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+  } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - 120) {
     $('nav').css({"background-color": "#222"});
+  } else if ($(this).scrollTop() >= $("#about").offset().top - 120) {
+    $('nav').css({"background-color": "#f8f9fa"});
   } else {
     $('nav').css({"background-color": "#f8f9fa"});
   }
