1880747ddadc874c96f93952c69f2612ab6c1354
Amelia <ameliakh2@gmail.com>
2019-02-12 18:32:08 +0000

    too tired for this sh**

--- js/navbar.js
+++ js/navbar.js
@@ -9,47 +9,46 @@ $(document).scroll(function() {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
     $('.navbar-brand').css({"color": "#222"});
     $('nav').css({"background-color": "#f8f9fa"});
-    $('a').css({"color": "#222"});
+    $('#topid > a').css({"color": "#222"});
     $('.klee-icon').attr("src","./assets/kleeIcon192x192.png");
 
   } else if ($(this).scrollTop() >= $("#pseudo").offset().top - overlap) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
     $('.navbar-brand').css({"color": "#f8f9fa"});
     $('nav').css({"background-color": "#222"});
-    $('a').css({"color": "#f8f9fa"});
+    $('#topid > a').css({"color": "#f8f9fa"});
     $('.klee-icon').attr("src","./assets/kleeIconWhite192x192.png");
 
   } else if ($(this).scrollTop() >= $("#resumable").offset().top - overlap) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
     $('.navbar-brand').css({"color": "#222"});
     $('nav').css({"background-color": "#f8f9fa"});
-    $('a').css({"color": "#222"});
+    $('#topid > a').css({"color": "#222"});
     $('.klee-icon').attr("src","./assets/kleeIcon192x192.png");
 
   } else if ($(this).scrollTop() >= $("#symbolic-execution").offset().top - overlap) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
     $('.navbar-brand').css({"color": "#f8f9fa"});
     $('nav').css({"background-color": "#222"});
-    $('a').css({"color": "#f8f9fa"});
+    $('#topid > a').css({"color": "#f8f9fa"});
     $('.klee-icon').attr("src","./assets/kleeIconWhite192x192.png");
 
   } else if ($(this).scrollTop() >= $("#about").offset().top - overlap) {
     $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
     $('.navbar-brand').css({"color": "#222"});
     $('nav').css({"background-color": "#f8f9fa"});
-    $('a').css({"color": "#222"});
+    $('#topid > a').css({"color": "#222"});
     $('.klee-icon').attr("src","./assets/kleeIcon192x192.png");
 
   } else {
-<<<<<<< HEAD
-    $('nav').css({"background-color": "#5d646d"});
-=======
-    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
-    $('.navbar-brand').css({"color": "#222"});
-    $('nav').css({"background-color": "#f8f9fa"});
-    $('a').css({"color": "#222"});
-    $('.klee-icon').attr("src","./assets/kleeIcon192x192.png");
 
->>>>>>> 7d2d63861d2479423772d9ede28148c11ebe676c
+    // $('nav').css({"background-color": "#5d646d"});
+
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
+    $('.navbar-brand').css({"color": "#f8f9fa"});
+    $('nav').css({"background-color": "#6d777f"});
+    $('#topid > a').css({"color": "#f8f9fa"});
+    $('.klee-icon').attr("src","./assets/kleeIconWhite192x192.png");
+
   }
 });
